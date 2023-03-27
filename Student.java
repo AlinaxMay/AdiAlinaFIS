d9fad64c6b2527c17d27bbe182a22bd9c424ff27
AlinaxMay <alina.marin@student.upt.ro>
2023-03-27 09:13:35 +0300

    Added the students Table and some dummy student list

--- Student.java
+++ Student.java
@@ -4,4 +4,20 @@ private String firstName;
 private String lastName;
 private int age;
 private double meanGrade;
+public Student(String firstName, String lastName, int age,
+double meanGrade) {
+//assignments
 }
+@Override
+public String toString() {
+//toString generated implementation
+}
+@Override
+public boolean equals(Object o) {
+//equals generated implementation
+}
+@Override
+public int hashCode() {
+//hashCode generated implementation
+}
+//generated getters and setters for all fields
